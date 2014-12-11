4942d763973e39d6b5a8317552f885811ab0deaf
Marshall Bowers <elliott.codes@gmail.com>
2014-12-11 17:30:48 -0500

    Non-working modules

---
+++ Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Problems.1
+
+main = do
+    printStrLn "hi"
---
+++ Problems/1.hs
@@ -0,0 +1,8 @@
+{-
+    Problem 1
+    Find the last element of a list.
+-}
+
+module Problems.1 where
+
+putStrLn "problem 1"
