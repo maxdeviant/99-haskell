eb11edc019c8b9a5dab1c0f683a5ad38f996e373
Marshall Bowers <elliott.codes@gmail.com>
2014-12-11 17:56:18 -0500

    Finish problem two

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import Problems.One
+import Problems.Two
 
 main :: IO()
 main = do
     problemOne
+    problemTwo
     putStrLn "Done"
---
+++ src/Problems/Two.hs
@@ -0,0 +1,16 @@
+{-
+    Problem 2
+    Find the last but one element of a list.
+-}
+
+module Problems.Two where
+
+myButLast list = if (tail (tail list)) == [] then head list else myButLast (tail list)
+
+problemTwo = do
+    putStrLn "Problem 2"
+    putStrLn "Find the last but one element of a list."
+    putStrLn "> myButLast [1, 2, 3, 4]"
+    print(myButLast [1, 2, 3, 4])
+    putStrLn "> myButLast ['a'..'z']"
+    print(myButLast ['a'..'z'])
