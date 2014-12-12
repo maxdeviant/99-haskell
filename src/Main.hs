d393d1fc537377ff44d185074e898b71e13a8aae
Marshall Bowers <elliott.codes@gmail.com>
2014-12-11 19:57:59 -0500

    Finish problem three

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Problems.One
 import Problems.Two
+import Problems.Three
 
 main :: IO()
 main = do
     problemOne
     problemTwo
+    problemThree
     putStrLn "Done"
---
+++ src/Problems/Three.hs
@@ -0,0 +1,16 @@
+{-
+    Problem 3
+    Find the K'th element of a list. The first element in the list is number 1.
+-}
+
+module Problems.Three where
+
+elementAt list index = list !! (index - 1)
+
+problemThree = do
+    putStrLn "Problem 3"
+    putStrLn "Find the K'th element of a list. The first element in the list is number 1."
+    putStrLn "> elementAt [1, 2, 3] 2"
+    print(elementAt [1, 2, 3] 2)
+    putStrLn "> elementAt \"haskell\" 5"
+    print(elementAt "haskell" 5)
