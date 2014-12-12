b97b908a972463b24e63f46bb2c5f60d31e85965
Marshall Bowers <elliott.codes@gmail.com>
2014-12-11 20:09:39 -0500

    Finish problem four

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,12 @@ module Main where
 import Problems.One
 import Problems.Two
 import Problems.Three
+import Problems.Four
 
 main :: IO()
 main = do
     problemOne
     problemTwo
     problemThree
+    problemFour
     putStrLn "Done"
---
+++ src/Problems/Four.hs
@@ -0,0 +1,18 @@
+{-
+    Problem 4
+    Find the number of elements of a list.
+-}
+
+module Problems.Four where
+
+myLength list = myLength' list 0
+    where myLength' [] _ = 0
+          myLength' (x:xs) length = if xs == [] then length + 1 else myLength' xs (length + 1)
+
+problemFour = do
+    putStrLn "Problem 4"
+    putStrLn "Find the number of elements of a list."
+    putStrLn "> myLength [123, 456, 789]"
+    print(myLength [123, 456, 789])
+    putStrLn "> myLength \"Hello, world!\""
+    print(myLength "Hello, world!")
