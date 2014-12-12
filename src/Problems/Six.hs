c329ed0760fc612c5882ad9111a482c84edd5d27
Marshall Bowers <elliott.codes@gmail.com>
2014-12-11 21:31:38 -0500

    Use precedence operator for print
    Who needs parentheses anyways?

--- src/Problems/Six.hs
+++ src/Problems/Six.hs
@@ -14,8 +14,8 @@ problemSix = do
     putStrLn "Problem 6"
     putStrLn "Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x)."
     putStrLn "> isPalindrome [1, 2, 3]"
-    print (isPalindrome [1, 2, 3])
+    print $ isPalindrome [1, 2, 3]
     putStrLn "> isPalindrome \"madamimadam\""
-    print (isPalindrome "madamimadam")
+    print $ isPalindrome "madamimadam"
     putStrLn "> isPalindrome [1, 2, 4, 8, 16, 8, 4, 2, 1]"
-    print (isPalindrome [1, 2, 4, 8, 16, 8, 4, 2, 1])
+    print $ isPalindrome [1, 2, 4, 8, 16, 8, 4, 2, 1]
--- src/Problems/Three.hs
+++ src/Problems/Three.hs
@@ -11,6 +11,6 @@ problemThree = do
     putStrLn "Problem 3"
     putStrLn "Find the K'th element of a list. The first element in the list is number 1."
     putStrLn "> elementAt [1, 2, 3] 2"
-    print (elementAt [1, 2, 3] 2)
+    print $ elementAt [1, 2, 3] 2
     putStrLn "> elementAt \"haskell\" 5"
-    print (elementAt "haskell" 5)
+    print $ elementAt "haskell" 5
